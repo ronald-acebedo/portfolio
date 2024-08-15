a4de0222a45126e8e4e00310a32ad50b29f69d27
Ronald Acebedo <ronaldbajaacebedo@gmail.com>
2024-08-15 19:04:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,17 @@ let sideBarList = document.getElementById('sidebar-list');
 
 sideBarToggle.addEventListener('click', () => {
  
-
+    // toggle sidebar list
     if(sideBarList) {
         sideBarList.classList.toggle('active');
     } 
 })
-
+    // remove sidebar list
 sideBarClose.addEventListener('click', () => {
     sideBarList.classList.remove('active');
 })
 
+// sidebar action
 const sideLink = document.querySelectorAll('.sidebar_link')
 
 const sideLinkaction = () => {
@@ -26,9 +27,26 @@ sideLink.forEach(n => n.addEventListener('click', sideLinkaction))
 const scrollBlurHeader = () => {
     const header = document.getElementById('header');
 
+    // when scroll > or = to 50, remove blur
     this.scrollY >= 50 ? header.classList.add('scroll_blur_header')
         : header.classList.remove('scroll_blur_header');
 
 }
-
 window.addEventListener('scroll', scrollBlurHeader); 
+
+// for darkmode
+let darkModeToggle = document.getElementById('moon-toggle');
+let body = document.querySelector('body')
+
+darkModeToggle.addEventListener('click', function() {
+    //when moon icon clicked, change it to sun
+    darkModeToggle.classList.toggle('fa-moon')
+
+    if(darkModeToggle.classList.toggle('fa-sun')) {
+        body.classList.toggle('darkmode_toggle')
+    } else {
+        body.classList.remove('darkmode_toggle')
+        body.style.transition = '0.5s'
+        
+    }
+})
